876ea5756b68a53b51e3e983cf58586e7782aaa9
mykyta_pavlov <43242475+mykyta-pavlov@users.noreply.github.com>
2020-03-17 11:05:29 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Driver;
+
+namespace MongoTable
+{
+    public partial class MainWindow : Window
+    {
+        static readonly MongoClient client = new MongoClient();
+        static readonly IMongoDatabase database = client.GetDatabase("local");
+        static readonly IMongoCollection<Student> collection = database.GetCollection<Student>("students");
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            MongoConnector();            
+        }
+
+        internal void MongoConnector()
+        {          
+            List<Student> studentsList = collection.AsQueryable().ToList();
+            foreach (Student student in studentsList)
+            {
+                DataGridXAML.Items.Add(student);
+            }
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student(TextBoxName.Text, TextBoxSurname.Text, TextBoxGroup.Text, int.Parse(TextBoxYearOfStudy.Text));
+            collection.InsertOne(student);
+            DataGridXAML.Items.Add(student);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoTable
+{
+    internal class Student
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("name")]
+        public string StudentName { get; set; }
+        [BsonElement("surname")]
+        public string StudentSurname { get; set; }
+        [BsonElement("group")]
+        public string StudentGroup { get; set; }
+        [BsonElement("yearOfStudy")]
+        public int StudentYearOfStudy { get; set; }
+
+        public Student(string studentName, string studentSurname, string studentGroup, int studentYearOfStudy)
+        {
+            StudentName = studentName;
+            StudentSurname = studentSurname;
+            StudentGroup = studentGroup;
+            StudentYearOfStudy = studentYearOfStudy;
+        }
+    }
+}
